644fda3e1294ba2f22eef61ae07b52fbe04d122e
Raphael <raphael.bonadia@shockit.com.br>
2024-03-26 11:13:09 -0300

    projeto em flutter/dart

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../components/meal_item.dart';
+import '../models/category.dart';
+import '../models/meal.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final List<Meal> meals;
+
+  const CategoriesMealsScreen(this.meals, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = meals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(category.title),
+      ),
+      backgroundColor: Theme.of(context).canvasColor,
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
+      ),
+    );
+  }
+}
